33ce0e252462e31de0b54811e502f5af63f419a6
sirin0762 <tlsdydwls123@gmail.com>
2022-08-10 20:41:12 +0900

    #14 - api 테스트 정의
    Spring Data REST + HAL Explorer 를 추가하여 API를 쉽게 작성하였다.

--- src/main/java/project/board/repository/ArticleCommentRepository.java
+++ src/main/java/project/board/repository/ArticleCommentRepository.java
@@ -1,8 +1,10 @@
 package project.board.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import project.board.domain.ArticleComment;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 
 }
