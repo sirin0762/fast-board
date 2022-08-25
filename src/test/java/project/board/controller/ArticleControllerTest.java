93e7ec5a48aa0f1eab15d0e0c020351c008c533b
sirin0762 <tlsdydwls123@gmail.com>
2022-08-26 07:18:17 +0900

    #18-게시글 뷰 기본 골격 작성
    컨트롤러, 뷰, 테스트까지 한 번에 적용

--- src/test/java/project/board/controller/ArticleControllerTest.java
+++ src/test/java/project/board/controller/ArticleControllerTest.java
@@ -38,14 +38,13 @@ class ArticleControllerTest {
             .andExpect(model().attributeExists("articles"));
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
         // given
 
         // when & then
-        mvc.perform(get("/article/1"))
+        mvc.perform(get("/articles/1"))
             .andExpect(status().isOk())
             .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
             .andExpect(view().name("articles/detail"))
