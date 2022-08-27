e8a0adaba3728365f2267d1aea497abc78da59bd
sirin0762 <tlsdydwls123@gmail.com>
2022-08-27 16:13:43 +0900

    #18-로그인 페이지 테스트 + 시큐리티 설정 기존 테스트에 반영
    로그인 페이지의 경우, Security 내장 페이지 이기 때문에 별도의 검사는 하지 않고,
    해당 경로에서 정상응답이 떨어지는지 확인한다.
    또한 기존의 테스트들에 인증 설정을 넣어주었다.

--- src/test/java/project/board/controller/ArticleControllerTest.java
+++ src/test/java/project/board/controller/ArticleControllerTest.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import project.board.configuration.SecurityConfiguration;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -16,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfiguration.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
