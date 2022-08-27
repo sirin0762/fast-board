ad56fd05003b7d18e08dae74efff4d90f83a57f5
sirin0762 <tlsdydwls123@gmail.com>
2022-08-27 15:01:43 +0900

    #19-Getter 와 index 추가

--- src/main/java/project/board/domain/UserAccount.java
+++ src/main/java/project/board/domain/UserAccount.java
@@ -1,15 +1,25 @@
 package project.board.domain;
 
+import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.Table;
 import java.util.Objects;
 
+@Getter
 @Entity
-@Table(name = "user_account")
+@Table(
+    name = "user_account",
+    indexes = {
+        @Index(columnList = "email", unique = true),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+    }
+)
 public class UserAccount extends BaseEntity {
 
     @Id
