75350f41c33ddc23a4c1ead9d0b2ba660b4fa16d
sirin0762 <tlsdydwls123@gmail.com>
2022-09-07 17:24:55 +0900

    #22-게시글 테스트 코드 적용
    게시글 기능 구현 전, 테스트를 먼저 작성하였다.
    그 과정에서 검색 유형이 필요하기 때문에 SearchType 을 만들었다.

---
+++ src/main/java/project/board/domain/type/SearchType.java
@@ -0,0 +1,5 @@
+package project.board.domain.type;
+
+public enum SearchType {
+    TITLE, CONTENT, ID, NICKNAME, HASHTAG
+}
---
+++ src/main/java/project/board/dto/ArticleDto.java
@@ -0,0 +1,17 @@
+package project.board.dto;
+
+import java.time.LocalDateTime;
+
+public record ArticleDto(
+    String title,
+    String content,
+    String hashtag,
+    LocalDateTime createdAt,
+    String createdBy
+) {
+
+    public static ArticleDto of(String title, String content, String hashtag, LocalDateTime createdAt, String createdBy) {
+         return new ArticleDto(title, content, hashtag, createdAt, createdBy);
+    }
+
+}
