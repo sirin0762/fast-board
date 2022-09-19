efb7523e2d40cce62cadff6cdb8fea11429a4bcf
sirin0762 <tlsdydwls123@gmail.com>
2022-09-20 08:24:07 +0900

    #22-서비스 로직 테스트 코드 적용 및 DTO 재설계
    서비스 로직의 나머지 테스트를 작성하고, 연관된 .DTO를 재설계했다.

--- src/main/java/project/board/dto/ArticleDto.java
+++ src/main/java/project/board/dto/ArticleDto.java
@@ -1,17 +1,46 @@
 package project.board.dto;
 
+import project.board.domain.Article;
+
 import java.time.LocalDateTime;
 
 public record ArticleDto(
+    Long id,
+    UserAccountDto userAccountDto,
     String title,
     String content,
     String hashtag,
     LocalDateTime createdAt,
-    String createdBy
+    String createdBy,
+    LocalDateTime updatedAt,
+    String updatedBy
 ) {
 
-    public static ArticleDto of(String title, String content, String hashtag, LocalDateTime createdAt, String createdBy) {
-         return new ArticleDto(title, content, hashtag, createdAt, createdBy);
+    public static ArticleDto of(Long id, UserAccountDto userAccountDto, String title, String content, String hashtag, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+         return new ArticleDto(id, userAccountDto, title, content, hashtag, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
+    public static ArticleDto from(Article entity) {
+        return new ArticleDto(
+            entity.getId(),
+            UserAccountDto.from(entity.getUserAccount()),
+            entity.getTitle(),
+            entity.getContent(),
+            entity.getHashtag(),
+            entity.getCreatedAt(),
+            entity.getCreatedBy(),
+            entity.getUpdatedAt(),
+            entity.getUpdatedBy()
+        );
+    }
+
+    public Article toEntity() {
+        return Article.of(
+            userAccountDto.toEntity(),
+            title,
+            content,
+            hashtag
+        );
     }
 
 }
---
+++ src/main/java/project/board/dto/UserAccountDto.java
@@ -0,0 +1,47 @@
+package project.board.dto;
+
+import project.board.domain.UserAccount;
+
+import java.time.LocalDateTime;
+
+public record UserAccountDto(
+    String userId,
+    String userPassword,
+    String email,
+    String nickname,
+    String memo,
+    LocalDateTime createdAt,
+    String createdBy,
+    LocalDateTime updatedAt,
+    String updatedBy
+) {
+
+    public static UserAccountDto of(String userId, String userPassword, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new UserAccountDto(userId, userPassword, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
+    public static UserAccountDto from(UserAccount entity) {
+        return new UserAccountDto(
+            entity.getUserId(),
+            entity.getUserPassword(),
+            entity.getEmail(),
+            entity.getNickname(),
+            entity.getMemo(),
+            entity.getCreatedAt(),
+            entity.getCreatedBy(),
+            entity.getUpdatedAt(),
+            entity.getUpdatedBy()
+        );
+    }
+
+    public UserAccount toEntity() {
+        return UserAccount.of(
+            this.userId,
+            this.userPassword,
+            this.email,
+            this.nickname,
+            this.memo
+        );
+    }
+
+}
