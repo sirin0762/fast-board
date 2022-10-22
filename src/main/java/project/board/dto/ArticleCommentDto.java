91dfadcf6db8bc4d8500bcbe08a6f1daf8584540
sirin0762 <tlsdydwls123@gmail.com>
2022-10-22 19:24:07 +0900

    #43 - Dto 정적팩토리 메서드 추가
    더 쉽게 dto를 만들수 있도록 수정하였다.

--- src/main/java/project/board/dto/ArticleCommentDto.java
+++ src/main/java/project/board/dto/ArticleCommentDto.java
@@ -2,6 +2,7 @@ package project.board.dto;
 
 import project.board.domain.Article;
 import project.board.domain.ArticleComment;
+import project.board.domain.UserAccount;
 
 import java.time.LocalDateTime;
 
@@ -15,6 +16,10 @@ public record ArticleCommentDto(
     LocalDateTime modifiedAt,
     String modifiedBy
 ) {
+
+    public static ArticleCommentDto of(Long articleId, UserAccountDto userAccountDto, String content) {
+        return new ArticleCommentDto(null, articleId, userAccountDto, content, null, null, null, null);
+    }
     public static ArticleCommentDto of(Long id, Long articleId, UserAccountDto userAccountDto, String content, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
         return new ArticleCommentDto(id, articleId, userAccountDto, content, createdAt, createdBy, modifiedAt, modifiedBy);
     }
@@ -32,7 +37,7 @@ public record ArticleCommentDto(
         );
     }
 
-    public ArticleComment toEntity(Article entity) {
-        return ArticleComment.of(userAccountDto.toEntity(), entity, content);
+    public ArticleComment toEntity(Article article, UserAccount userAccount) {
+        return ArticleComment.of(userAccount, article, content);
     }
 }
