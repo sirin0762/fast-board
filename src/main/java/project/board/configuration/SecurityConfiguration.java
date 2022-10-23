faa0b19e0b096520da779c93c55806919f0c3b3d
sirin0762 <tlsdydwls123@gmail.com>
2022-10-23 20:25:10 +0900

    #45 - Security,Configuration 구현
    인증에 필요한 Security 설정을 구현했다.

--- src/main/java/project/board/configuration/SecurityConfiguration.java
+++ src/main/java/project/board/configuration/SecurityConfiguration.java
@@ -1,9 +1,20 @@
 package project.board.configuration;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import project.board.dto.UserAccountDto;
+import project.board.dto.security.UserPrincipal;
+import project.board.repository.UserAccountRepository;
 
 @Configuration
 public class SecurityConfiguration {
@@ -11,9 +22,36 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-            .formLogin().and()
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .mvcMatchers(
+                    HttpMethod.GET,
+                    "/",
+                    "/articles",
+                    "/articles/search-hashtag"
+                ).permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin()
+                .and()
+            .logout()
+                .logoutSuccessUrl("/")
+                .and()
             .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
+        return (username -> userAccountRepository.findById(username)
+                .map(UserAccountDto::from)
+                .map(UserPrincipal::from)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username: " + username))
+        );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
